aefeecea93318810e229663b3df233d6af45c662
Rushikesh <rushikeshdasarwad23@gmail.com>
2024-02-21 16:36:29 +0530

    Updated Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primary": "#141414",
-        "blue": "#3575E2"
-      }
+        primary: "#141414",
+        blue: "#f4932a",
+      },
     },
   },
   plugins: [],
-}
-
+};
